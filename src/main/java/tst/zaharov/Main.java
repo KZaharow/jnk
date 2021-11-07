b54763a90d7603cae9e3908f6ac3a13c036fb226
zaharov <KZaharow@gmail.com>
2021-11-07 18:44:04 +0300

    change 4

--- src/main/java/tst/zaharov/Main.java
+++ src/main/java/tst/zaharov/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         Stream.generate(Gretting::new).limit(100).forEach(Gretting::getGreeting);
+        Stream.generate(Gretting::new).limit(100).forEach(Gretting::getGreeting);
     }
 }
